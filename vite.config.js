aeb1f6a7bc76f4cf60be4ebb45797ec962c028c3
Michael Cortese <mcortese2@wisc.edu>
2025-12-16 15:49:06 -0600

    fix build

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: '/Prop_Hunt/', // GitHub Pages base path
+    publicDir: 'public', // Explicitly set public directory
 });
